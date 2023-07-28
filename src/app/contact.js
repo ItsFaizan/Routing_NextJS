99f562da79d52ff3d39c9dd3a3647e0d52f39ed8
ItsFaizan <faizanjavid71@gmail.com>
2023-07-28 20:46:52 +0500

    Basic Routing done

---
+++ src/app/contact.js
@@ -0,0 +1,12 @@
+
+
+
+function contact () {
+  return (
+    <div>
+       <h1>contact</h1> 
+    </div>
+  )
+}
+
+export default contact
---
+++ src/app/product/page.tsx
@@ -0,0 +1,12 @@
+
+function ProductList()  {
+  return (
+    <div>
+      ProductList
+      <h1>Ji</h1>
+      </div>
+  )
+}
+
+export default ProductList
+
